d638593982a54ebb4366ebeca43369da0f3c1b18
Asmundur <asmundalmenning@gmail.com>
2024-05-01 04:12:31 +0200

    switched the inputs for better use of mapping

--- src/UnitLister.hs
+++ src/UnitLister.hs
@@ -1,10 +1,10 @@
 module UnitLister where
 import Traits
 import Units
-import Data.Char (toUpper, toLower)
+import Data.Char (toLower)
 
-unitLister :: Either TraitName Int -> [Unit] -> (Either TraitName Int,[Unit])
-unitLister (Left trait) unitList = (Left trait, filter (traitFinder trait) unitList)
+unitLister :: [Unit] -> Either TraitName Int -> [Unit]
+unitLister unitList (Left trait) = filter (traitFinder trait) unitList
     where traitFinder trait unit = map toLower trait `elem` map (map toLower) (Units.traits unit)
-unitLister (Right cost) unitList = (Right cost, filter (costFinder cost) unitList)
+unitLister unitList (Right cost)  = filter (costFinder cost) unitList
     where costFinder cost unit = cost == Units.cost unit
