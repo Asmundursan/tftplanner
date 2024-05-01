349c4cd2dc7173deeaa44994a29b0164db4940ca
Asmundur <asmundalmenning@gmail.com>
2024-05-01 03:49:30 +0200

    fixed the lister not being able to list all traittypes

--- src/UnitLister.hs
+++ src/UnitLister.hs
@@ -5,6 +5,6 @@ import Data.Char (toUpper, toLower)
 
 unitLister :: Either TraitName Int -> [Unit] -> (Either TraitName Int,[Unit])
 unitLister (Left trait) unitList = (Left trait, filter (traitFinder trait) unitList)
-    where traitFinder trait unit = (toUpper(head trait) : map toLower(tail trait)) `elem` Units.traits unit
+    where traitFinder trait unit = map toLower trait `elem` map (map toLower) (Units.traits unit)
 unitLister (Right cost) unitList = (Right cost, filter (costFinder cost) unitList)
     where costFinder cost unit = cost == Units.cost unit
